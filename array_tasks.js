e9ebf3c407f9ee4a8f5603f98fc9523369da8a88
Kris <kristopher_anderson@icloud.com>
2018-02-14 21:51:33 +0000

    return array of index postions.

--- array_tasks.js
+++ array_tasks.js
@@ -1,6 +1,6 @@
 const arrayTasks = {
 
-	concat: function (arr1, arr2) {
+  concat: function (arr1, arr2) {
     return arr1.concat(arr2);
   },
 
@@ -9,7 +9,7 @@ const arrayTasks = {
     return arr;
   },
 
-// https://stackoverflow.com/questions/19645186/square-each-number-in-an-array-in-javascript
+  // https://stackoverflow.com/questions/19645186/square-each-number-in-an-array-in-javascript
   // function (arr) {
   //   return arr.map(function (x) {
   //     return Math.pow(x, 2);
@@ -17,38 +17,46 @@ const arrayTasks = {
   // }
 
   square: function (arr) {
-    return arr.map( function (number) {
+    const newArray = arr.map( function (number) {
       return Math.pow(number, 2);
     });
-  }
+    return newArray;
+  },
 
-  // sum: function (arr) {
-  //     const total = arr.reduce(function(runningTotal, number){
-  //     return runningTotal + number;
-  //     }, 0)
-  //
-  //     return total;
-  //   },
+  sum: function (arr) {
+    return arr.reduce( function (runningTotal, currentItem) {
+      return runningTotal + currentItem;
+    }, 0);
+  },
+
+  // removeAndClone: function (arr, valueToRemove) {
+
+  // },
+
+  findIndexesOf: function (arr, itemToFind) {
+    const indexArray = [];
+    arr.forEach(function(number, index){
+      if(number === itemToFind){
+        indexArray.push(index);
+      }
+    });
+    return indexArray;
+  },
+
+  // sumOfAllEvenNumbersSquared: function (arr) {
+  //   const filteredArray = arr.filter(function(number){
+  //     return number % 2 === 0;
+  //   });
+  //   return filteredArray;
+  // }
 
 
 
-	// removeAndClone: function (arr, valueToRemove) {
+  // ----------- EXTENSION ------------
 
-	// },
+  // findDuplicates: function (arr) {
 
-	// findIndexesOf: function (arr, itemToFind) {
-
-	// },
-
-	// sumOfAllEvenNumbersSquared: function (arr) {
-
-	// }
-
-	// ----------- EXTENSION ------------
-
-	// findDuplicates: function (arr) {
-
-	// },
+  // },
 
 }
 
